a2d95d30e34a190014573012870b7687d011f978
Mark Joshua Orcullo <orcullomark856@gmail.com>
2025-06-03 20:02:45 +0800

    Update enemy_1.gd

--- scripts/enemy_1.gd
+++ scripts/enemy_1.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var word: String = ""
-var velocity: float = 100.0  # Pixels per second
+var velocity: float = 100.0
 var is_moving: bool = true
 
 func _ready() -> void:
@@ -9,20 +9,17 @@ func _ready() -> void:
 	$RayCast2D.enabled = true
 
 func _process(delta: float) -> void:
+	if $RayCast2D.is_colliding():
+		var collider = $RayCast2D.get_collider()
+		if collider and (collider.is_in_group("castle") or collider.is_in_group("enemy")):
+			is_moving = false
+		else:
+			is_moving = true  # Not a valid blocker, keep moving
+	else:
+		is_moving = true  # No collision, keep moving
+
 	if is_moving:
 		position.y += velocity * delta
-		
-		if $RayCast2D.is_colliding():
-			var collider = $RayCast2D.get_collider()
-			if collider is Node and collider.is_in_group("castle"):
-				is_moving = false 
-				print("Collide with castle")
-				# play attack animation
-			else:
-				is_moving = false 
-				# play idle
-		else:
-			is_moving = true
 
 func get_word() -> String:
 	return word.to_upper()
