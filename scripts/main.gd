b3e9f4b7c98db2a698834d58f3282f904e8c9b08
Mark Joshua Orcullo <orcullomark856@gmail.com>
2025-06-05 15:48:35 +0800

    added plant 1

--- scripts/main.gd
+++ scripts/main.gd
@@ -23,7 +23,7 @@ var last_spawn_position = 0
 var velocity:float = 100.0
 
 # Enemies
-const ENEMY_1 = preload("res://nodes/enemy4.tscn")
+const ENEMY_1 = preload("res://nodes/enemy5.tscn")
 
 @onready var game_manager: Node = $GameManager
 @onready var typing_container: Control = $Typingcontainer
