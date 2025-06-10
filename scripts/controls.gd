b12eb7e43aaced3f3aaec0a03aea3f81a498d6c7
Mark Joshua Orcullo <orcullomark856@gmail.com>
2025-06-11 04:50:21 +0800

    added conttrol page

---
+++ scripts/controls.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_closebtn_mouse_entered() -> void:
+	$closebtn.modulate = Color(1.5, .8, .8)
+
+
+func _on_closebtn_mouse_exited() -> void:
+	$closebtn.modulate = Color(1,1,1)
+
+
+func _on_closebtn_pressed() -> void:
+	for i in range(15):
+		modulate.a = lerp(1.0, 0.0, i / 15.0)
+		await get_tree().create_timer(0.02).timeout
+	queue_free()
